53eb1fef5287c528bd93f17a2194a209cdbcf0b3
Danni Ke <dxk5418@psu.edu>
2020-01-27 17:05:56 -0500

    longest_Common_Prefix_14 update

--- src/com/example/longest_Common_Prefix_14.java
+++ src/com/example/longest_Common_Prefix_14.java
@@ -2,8 +2,17 @@ package com.example;
 
 public class longest_Common_Prefix_14 {
     public String longestCommonPrefix(String[] strs) {
-
-
-        return "";
+        //compare 1&2, (12)&3, (123)&4.......
+        if(strs.length == 0) return "";
+        String prefix = strs[0];
+        for(int i =1; i<strs.length; i++)
+        {
+            while(strs[i].indexOf(prefix)!=0)
+            {
+                prefix = prefix.substring(0, prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
     }
 }
